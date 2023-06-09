fcde2928f321deeb4ea61099c98f39a5fb643812
ziqi <wangziqi000@126.com>
2023-06-09 11:46:38 -0700

    earlyfusion_allmodalities

---
+++ mmtrack/models/mocap/__init__.py
@@ -0,0 +1,6 @@
+from .decoderv3 import DecoderMocapModel
+from .single import SingleModalityModel
+from .single import ModalityEncoder
+from .output_head import OutputHead
+from .oracle import OracleModel
+from .kfdetr import KFDETR
---
+++ src/mmdetection/tools/detr/collect.sh
@@ -0,0 +1,18 @@
+START=$PWD
+
+cd $WORK 
+
+singularity run --nv -H $WORK --bind /work:/work $WORK/python.sif python \
+    $WORK/mmdetection/tools/detr/collect_output.py \
+    $WORK/detr_r50_16x4_5e_output_heads_only/detr_r50_4x16_5e_output_heads_only_seed_0/detr_r50_4x16_5e_output_heads_only.py \
+    /work/meet_and_colin_umass_edu/exps/detr_r50_16x4_5e_output_heads_only/detr_r50_4x16_5e_output_heads_only_seed_0/latest.pth \
+    /work/meet_and_colin_umass_edu/exps/detr_r50_16x4_5e_output_heads_only/detr_r50_4x16_5e_output_heads_only_seed_0/output.pkl
+
+
+#singularity run --nv -H $WORK --bind /work:/work $WORK/python.sif python \
+    #$WORK/mmdetection/tools/detr/coco_eval_from_pkl.py \
+    #/work/meet_and_colin_umass_edu/exps/detr_r50_16x4_5e_output_heads_only/detr_r50_4x16_5e_output_heads_only_seed_0/output.pkl \
+    #/work/meet_and_colin_umass_edu/exps/detr_r50_16x4_5e_output_heads_only/detr_r50_4x16_5e_output_heads_only_seed_0/results.json 
+
+
+cd $START
